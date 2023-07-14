cbffad1836c4bcb9e7b8e6107a983f515ae1f35d
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-07-14 14:21:47 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const version = require("./package.json");
+const c = require("./commands/math/conv.js")
+const d = require("./commands/math/decide.js")
+const m = require("./commands/math/math.js");
+const r = require("./commands/math/random.js")
+
+module.exports = {
+        px: 'c#',
+        playing: `discord.js v${version.dependencies["discord.js"].replace('^','')} | mathjs v${version.dependencies["mathjs"].replace('^','')}`,
+        ccmds: c.cmds,
+        dcmds: d.cmds,
+        cmds: m.cmds,
+        rcmds: r.cmds, 
+};
---
+++ main.js
@@ -0,0 +1,55 @@
+const { Client, Intents, Collection } = require('discord.js');
+const { readdirSync } = require('fs');
+
+let client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ],
+    disableMentions: 'everyone',
+});
+
+client.config = require('./config');
+client.commands = new Collection();
+
+const events = readdirSync('./events/').filter(file => file.endsWith('.js'));
+for (const file of events) {
+    const event = require(`./events/${file}`);
+    console.log(`-> Loaded event ${file.split('.')[0]}`);
+    client.on(file.split('.')[0], event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+};
+console.log(`-> Loaded commands...`);
+readdirSync('./commands/').forEach(dirs => {
+    const commands = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        console.log(`${command.name.toLowerCase()} Load Command!`);
+        client.commands.set(command.name.toLowerCase(), command);
+        delete require.cache[require.resolve(`./commands/${dirs}/${file}`)];
+    };
+});
+    
+const express = require("express");
+const app = express();
+const http = require("http");
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 60000);
+
+if(process.env.TOKEN){
+client.login(process.env.TOKEN).catch(e => {
+console.log("The Bot Token You Entered Into Your Project Is Incorrect Or Your Bot's INTENTS Are OFF!")
+})
+} else {
+console.log("Please Write Your Bot Token Opposite The Token In The .env File In Your Project!")
+}
+
+
+client.login(process.env.TOKEN);
