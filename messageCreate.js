5ae0bf28ca7acfbc2d8b5cd9e6661b27c3f361da
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-07-14 14:06:31 +0900

    Add files via upload

---
+++ messageCreate.js
@@ -0,0 +1,24 @@
+const Database = require('better-sqlite3');
+const db = new Database('prefixes.db');
+
+module.exports = (client, message) => {
+    if (message.author.bot || message.channel.type === 'dm') return;
+
+    const guildId = message.guild.id;
+    const prefix = getPrefix(guildId) || client.config.px;
+      
+    function getPrefix(guildId) {
+      const query = db.prepare('SELECT prefix FROM prefixes WHERE guild_id = ?');
+      const result = query.get(guildId);
+      return result ? result.prefix : null;
+    };
+
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
+
+    if (cmd) cmd.execute(client, message, args);
+};
---
+++ ready.js
@@ -0,0 +1,33 @@
+module.exports = async (client) => {
+    console.log(`${client.user.username} Login!`);
+    const users = client.guilds.cache
+    .map((guild) => guild.memberCount)
+    .reduce((p, c) => p + c);
+  const statuses = [
+    {
+      name: `${client.config.px}help | ${client.guilds.cache.size}servers`,
+      type: 'WATCHING',
+    },
+    {
+      name: `${client.config.px}help | ${users}users`,
+      type: 'WATCHING',
+    },
+    {
+      name: `${client.config.playing}`,
+      type: 'PLAYING',
+    },
+    {
+      name: `Math`,
+      type: `PLAYING`,
+    }
+  ];
+
+  let statusIndex = 0;
+
+  setInterval(() => {
+    client.user.setPresence({
+      activities: [statuses[statusIndex]]
+    });
+    statusIndex = (statusIndex + 1) % statuses.length;
+  }, 10000);
+};
