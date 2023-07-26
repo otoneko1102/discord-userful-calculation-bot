e443c5987c08bc3e7b481f2319f36bba11b23fd7
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-07-26 10:39:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const { Client, Intents, Collection } = require('discord.js');
 const { readdirSync } = require('fs');
 
-let client = new Client({
+const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
+        Intents.FLAGS.GUILD_MESSAGES
     ],
     disableMentions: 'everyone',
 });
