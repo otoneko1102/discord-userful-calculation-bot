4d4f81f8e57761e44493a8ddc6aad2a07ee1283a
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-07-27 09:01:52 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,16 @@ const c = require("./commands/math/conv.js")
 const d = require("./commands/math/decide.js")
 const m = require("./commands/math/math.js");
 const r = require("./commands/math/random.js")
-const s = require("./commands/math/script.js")
+const s = require("./commands/math/shape.js")
+const scr = require("./commands/other/script.js")
 
 module.exports = {
-        px: '', //set prefix
-        playing: `discord.js v${version.dependencies["discord.js"].replace('^','')} | mathjs v${version.dependencies["mathjs"].replace('^','')}`,
-        ccmds: c.cmds,
-        dcmds: d.cmds,
-        cmds: m.cmds,
-        rcmds: r.cmds,
-        scmds: s.cmds,
+  px: 'c#',
+  playing: `discord.js v${version.dependencies["discord.js"].replace('^','')} | mathjs v${version.dependencies["mathjs"].replace('^','')}`,
+  ccmds: c.cmds,
+  dcmds: d.cmds,
+  cmds: m.cmds,
+  rcmds: r.cmds,
+  scmds: s.cmds,
+  scrcmds: scr.cmds
 };
