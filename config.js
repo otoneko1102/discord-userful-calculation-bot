0ffabb45bbc2a15093781b328359017dc4d4735b
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-07-29 00:02:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const s = require("./commands/math/shape.js")
 const scr = require("./commands/other/script.js")
 
 module.exports = {
-  px: '', //Set prefix here.
+  px: '', // Set prefix here.
   playing: `discord.js v${version.dependencies["discord.js"].replace('^','')} | mathjs v${version.dependencies["mathjs"].replace('^','')}`,
   ccmds: c.cmds,
   dcmds: d.cmds,
