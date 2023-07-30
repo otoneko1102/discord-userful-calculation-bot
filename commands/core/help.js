2ebda4300cb24e205869976c28b69fedd556e878
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-07-30 20:06:12 +0900

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -11,6 +11,11 @@ module.exports = {
 
     execute(client, message, args) {
       const embed = new MessageEmbed();
+      const c_err_embed = new MessageEmbed()
+                 .setTitle(`Failed: Error.`)
+                 .setFooter({ text: `{} = Required,() = Optional` })
+                 .addField(`Usage example`,`\`${prefix}help\``)
+                 .setColor('RED');
       const btn = new MessageButton();
       btn.setLabel('Invite bot');
       btn.setStyle('LINK');
@@ -28,14 +33,18 @@ module.exports = {
           return null;
         }
       };
-
+      
+      try{
         embed.setColor('BLUE');
         embed.setTitle(client.user.username);
         const commands = client.commands.filter(x => x.showHelp !== false);
         embed.setDescription(client.config.playing.replace(/ \| /g,'\n'))
         embed.addField(`${commands.size} commands\n➥${client.config.ccmds.length} conv types\n➥${client.config.dcmds.length} decide types\n➥${client.config.cmds.length} math types\n➥${client.config.rcmds.length} random types\n➥${client.config.scmds.length} shape types\n➥${client.config.scrcmds.length} script languages`, commands.map(x => `**${x.utilisation.replace(/{prefix}/g,prefix)}** ${x.aliases[0] ? ` (aliase: **${x.aliases.map(y => y).join('/')}**)` : ''}\n${x.description ?? '---'}`).join('\n'));
         embed.setFooter({text: `{} = Required,() = Optional\nUsage example: ${prefix}{command}`})
-        
+      
         message.reply({ embeds: [embed], components: [row] });
+      }catch{
+        message.reply({embeds: [c_err_embed]})
+      }
     },
 };
