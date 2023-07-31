9885bef72d7dcec7ecabbfb2b4e6ab11aab3094f
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-08-01 08:21:25 +0900

    Update ping.js

--- commands/core/ping.js
+++ commands/core/ping.js
@@ -1,4 +1,6 @@
 const { MessageEmbed } = require("discord.js");
+const Database = require('better-sqlite3');
+const db = new Database('prefixes.db');
 
 module.exports = {
     name: 'ping',
@@ -8,11 +10,33 @@ module.exports = {
     utilisation: '{prefix}ping',
 
     execute(client, message) {
+      const guildId = message.guild.id;
+      const prefix = getPrefix(guildId) || client.config.px;
+      
+      function getPrefix(guildId) {
+        try{
+          const query = db.prepare('SELECT prefix FROM prefixes WHERE guild_id = ?');
+          const result = query.get(guildId);
+          return result ? result.prefix : null;
+        } catch {
+          return null;
+        }
+      };
+      
       const embed = new MessageEmbed()
         .setTitle('Pong.')
         .addField(`Web Socket`,`**${client.ws.ping}ms**`)
         .addField(`API Endpoint`,`**${Date.now() - message.createdTimestamp}ms**`)
         .setColor('GREEN')
-      message.reply({embeds: [embed]});
+      const c_err_embed = new MessageEmbed()
+                 .setTitle(`Failed: Error.`)
+                 .setFooter({ text: `{} = Required,() = Optional` })
+                 .addField(`Usage example`,`\`${prefix}ping\``)
+                 .setColor('RED');
+      try{
+        message.reply({embeds: [embed]});
+      }catch{
+        message.reply({embeds: [c_err_embed]});
+      }
     },
 };
