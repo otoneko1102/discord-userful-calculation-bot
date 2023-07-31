c0ff3b6d6e4173e965c92da1321c025ffb680c6b
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2023-08-01 08:30:46 +0900

    Update info.js

--- commands/core/info.js
+++ commands/core/info.js
@@ -29,16 +29,23 @@ module.exports = {
         }
       };
       
-        embed.setColor('BLUE');
-        embed.setTitle(client.user.username);
-        const commands = client.commands.filter(x => x.showHelp !== false);
-        embed.addField(`ID`,client.user.id);
-        embed.addField(`Version`,client.config.playing.replace(/ \| /g,'\n'));
-        embed.addField(`Prefix`,`**${prefix}** (Default: **${client.config.px}**)`);
-        embed.addField(`Number of commands`,`${commands.size} commands\n➥${client.config.ccmds.length} conv types\n➥${client.config.dcmds.length} decide types\n➥${client.config.cmds.length} math types\n➥${client.config.rcmds.length} random types\n➥${client.config.scmds.length} shape types\n➥${client.config.scrcmds.length} script languages`);
-        embed.addField(`Number of servers`,`${client.guilds.cache.size}servers`);
-        const users = client.guilds.cache.map((guild) => guild.memberCount).reduce((p, c) => p + c);
-        embed.addField(`Number of users`,`${users}users`)
-        message.reply({ embeds: [embed], components: [row] });
+      embed.setColor('BLUE');
+      embed.setTitle(client.user.username);
+      const commands = client.commands.filter(x => x.showHelp !== false);
+      embed.addField(`ID`,client.user.id);
+      embed.addField(`Version`,client.config.playing.replace(/ \| /g,'\n'));
+      embed.addField(`Prefix`,`**${prefix}** (Default: **${client.config.px}**)`);
+      embed.addField(`Number of commands`,`${commands.size} commands\n➥${client.config.ccmds.length} conv types\n➥${client.config.dcmds.length} decide types\n➥${client.config.cmds.length} math types\n➥${client.config.rcmds.length} random types\n➥${client.config.scmds.length} shape types\n➥${client.config.scrcmds.length} script languages`);
+      embed.addField(`Number of servers`,`${client.guilds.cache.size}servers`);
+      let users;
+      try{
+        users = client.guilds.cache
+        .map((guild) => guild.memberCount)
+        .reduce((p, c) => p + c);
+      }catch{
+        users = null;
+      }
+      embed.addField(`Number of users`,`${users}users`)
+      message.reply({ embeds: [embed], components: [row] });
     },
 };
